451c62b080b0cf5d99166b8562321c1c45116227
Fang Yilin <935474206@qq.com>
2017-08-03 14:41:15 +0800

    Add the Item-Test scene

---
+++ Assets/Scripts/Item/TransformController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformController : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown("w"))
+        {
+            obj.transform.Translate(0, 0.1f, 0);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            obj.transform.Translate(0.1f, 0, 0);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            obj.transform.Translate(0, -0.1f, 0);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            obj.transform.Translate(-0.1f, 0, 0);
+        }
+    }
+}
