bd7a2beab21cbc8d7d5ed30ab747086843f793a6
= <947426443@qq.com>
2017-08-03 16:05:50 +0800

    map init.

---
+++ Assets/Scripts/Map/ItemTagger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTagger : MonoBehaviour
+{
+	public float lifetime;
+	
+	float t;
+	
+	void Start()
+	{
+		t = lifetime;
+	}
+	
+	void Update()
+	{
+		t -= Time.deltaTime;
+		if(t <= 0f)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+	
+	
+}
