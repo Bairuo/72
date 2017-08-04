22076a577478ffc6c3bab5b67435bc3980b13ac3
= <947426443@qq.com>
2017-08-04 15:10:34 +0800

    audio scripts.

--- Assets/Scripts/Map/GlobalAudioController.cs
+++ Assets/Scripts/Map/GlobalAudioController.cs
@@ -4,6 +4,43 @@ using UnityEngine;
 
 public class GlobalAudioController : MonoBehaviour
 {
+	public float timelast;
+	public float timedelay;
+	public AudioSource currentAudioSource;
+	public AudioSource nextAudioSource;
 	
+	float t;
+	float cr;
+	float rate;
+	
+	float curBeginV;
+	float nxtEndV;
+	
+	void Start()
+	{
+		curBeginV = currentAudioSource.volume;
+		nxtEndV = nextAudioSource.volume;
+	}
+	
+	void Update()
+	{
+		t += Time.deltaTime;
+		if(t >= timelast)
+		{
+			cr = timedelay + timelast - t;
+			rate = cr / timedelay;
+			nextAudioSource.enabled = true;
+			
+			currentAudioSource.volume = rate * curBeginV;
+			nextAudioSource.volume = (1 - rate) * nxtEndV;
+			if(rate > 1f)
+			{
+				currentAudioSource.enabled = false;
+				currentAudioSource.volume = 0f;
+				nextAudioSource.volume = nxtEndV;
+				Destroy(this);
+			}
+		}
+	}
 	
 }
