2e6024810e2be6c65e7722ef7a602d1c781d46a0
Fang Yilin <935474206@qq.com>
2017-08-04 21:23:24 +0800

    add the main sence ui

---
+++ Assets/Scripts/NetController/LevelController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+    public Image lv1;
+    public Image lv2;
+    public Image lv3;
+    public Image Lv3;
+    public int level;
+	// Use this for initialization
+	void Start () {
+        level = 1;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        switch (level)
+        {
+            case 1:
+                LoadLevel1();
+                break;
+            case 2:
+                LoadLevel2();
+                break;
+            case 3:
+                LoadLevel3();
+                break;
+            case 4:
+                LoadLevel4();
+                break;
+
+        }
+                
+	}
+
+    public void LoadLevel1()
+    {
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.5f);
+        lv1.color = new Color(1, 1, 0.3f);
+        lv1.color = new Color(1, 1, 0.2f);
+    }
+
+    public void LoadLevel2()
+    {
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.3f);
+        lv1.color = new Color(1, 1, 0.2f);
+    }
+
+    public void LoadLevel3()
+    {
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.2f);
+    }
+
+    public void LoadLevel4()
+    {
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.8f);
+        lv1.color = new Color(1, 1, 0.8f);
+    }
+
+}
---
+++ Assets/Scripts/NetController/hpbar1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpbar1 : MonoBehaviour {
+
+    public Image front;
+    public GameObject speedLevelController;
+    public GameObject massLevelController;
+    private GameObject hostPlayer;
+
+	// Use this for initialization
+	void Start () {
+        front.fillAmount = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!hostPlayer)
+        {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
+            if(objs != null)
+            {
+                foreach(GameObject obj in objs)
+                {
+                    if(obj.GetComponent<PlayerController>().IsMine())
+                    {
+                        hostPlayer = obj;
+                        front.fillAmount = hostPlayer.GetComponent<PlayerController>().health / 100;
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            front.fillAmount = hostPlayer.GetComponent<PlayerController>().health / 100;
+            speedLevelController.GetComponent<LevelController>().level = hostPlayer.GetComponent<PlayerController>().SpeedLevel;
+            massLevelController.GetComponent<LevelController>().level = hostPlayer.GetComponent<PlayerController>().MassLevel;
+        }
+    }
+}
