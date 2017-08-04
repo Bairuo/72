c05b23a05c5f3181a0b7952895e50f0f7c4a0c86
Fang Yilin <935474206@qq.com>
2017-08-05 00:33:21 +0800

    fix the appearance of award

--- Assets/Scripts/Player/AwardController.cs
+++ Assets/Scripts/Player/AwardController.cs
@@ -9,27 +9,28 @@ public class AwardController : MonoBehaviour {
     public GameObject mu;
     public GameObject md;
 
-    public float time;
-    private float timer;
-
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        timer -= Time.deltaTime;
-	}
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject gbj = collision.gameObject;
-        if(gbj.GetComponent<SpeedUp>())
+        if (gbj.GetComponent<SpeedUp>())
         {
-            GameObject obj = Object.Instantiate(su,this.gameObject.transform);
-            obj.transform.Translate(0, 1f, 0);
+            GameObject obj = Object.Instantiate(su, this.gameObject.transform);
+            obj.transform.Translate(0, 0, -1);
+        }
+        if (gbj.GetComponent<SlowDown>())
+        {
+            GameObject obj = Object.Instantiate(sd, this.gameObject.transform);
+            obj.transform.Translate(0, 0, -1);
+        }
+        if (gbj.GetComponent<MassUp>())
+        {
+            GameObject obj = Object.Instantiate(mu, this.gameObject.transform);
+            obj.transform.Translate(0, 0, -1);
+        }
+        if (gbj.GetComponent<MassDown>())
+        {
+            GameObject obj = Object.Instantiate(md, this.gameObject.transform);
+            obj.transform.Translate(0, 0, -1);
         }
     }
 }
