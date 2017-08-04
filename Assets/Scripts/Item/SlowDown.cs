c12accb773f646933a4b55cf14bb6b355fd67c66
Fang Yilin <935474206@qq.com>
2017-08-04 15:59:09 +0800

    add some imtems

--- Assets/Scripts/Item/SlowDown.cs
+++ Assets/Scripts/Item/SlowDown.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SlowDown : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<PlayerController>().SpeedLevel > 1)
+            {
+                collision.gameObject.GetComponent<PlayerController>().SpeedLevel--;
+            }
+            Destroy(this.gameObject);
+        }
+    }
 }
--- Assets/Scripts/Item/SpeedUp.cs
+++ Assets/Scripts/Item/SpeedUp.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeedUp : MonoBehaviour {
-    public float speed;
+    public float speedSize;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            collision.gameObject.GetComponent<PlayerController>().ChangeSpeed(speed);
+            if(collision.gameObject.GetComponent<PlayerController>().SpeedLevel < 4)
+            {
+                collision.gameObject.GetComponent<PlayerController>().SpeedLevel++;
+            }
             Destroy(this.gameObject);
         }
     }
