379184baf5486d64a1fce8cda32b2337991c699e
Zhengru Jiang <jzr98@qq.com>
2017-08-04 22:39:00 +0700

    Add many thing

---
+++ Assets/Scripts/SceneManager/CurtainController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurtainController : MonoBehaviour {
+
+    public void EnterNetStart()
+    {
+        Application.LoadLevel("NetStart");
+    }
+}
