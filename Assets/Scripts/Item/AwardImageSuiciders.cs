5ef412741cb0310883c1ba4fc0a17fedfe32e850
Fang Yilin <935474206@qq.com>
2017-08-05 10:17:45 +0800

    settings...

--- Assets/Scripts/Item/AwardImageSuiciders.cs
+++ Assets/Scripts/Item/AwardImageSuiciders.cs
@@ -14,6 +14,7 @@ public class AwardImageSuiciders : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        this.gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
         timer -= Time.deltaTime;
         if(timer <= 0)
         {
