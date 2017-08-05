72482c23825dbc43869d7a628d82cfb07ccf0c47
Fang Yilin <935474206@qq.com>
2017-08-05 10:27:30 +0800

    fixed the OpenView

--- Assets/Scripts/Item/OpenView.cs
+++ Assets/Scripts/Item/OpenView.cs
@@ -6,7 +6,7 @@ public class OpenView : MonoBehaviour {
     public float time;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player"  && collision.gameObject.GetComponent<PlayerController>().IsMine())
         {
             GameObject.FindWithTag("fog").GetComponent<FogController>().MakeVisible(time);
         }
